bd9ef44d32e82a26e4fe21a9a97e1e2bc260ad58
David <david.ramis89@gmail.com>
2020-07-02 15:08:48 +0200

    fix API call

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const button = $('button');
-const input = $('.search');
+const button = $('.button');
+var input = $('#search-input');
+const lyricsContainer = $('.lyrics-container');
+const lyricsList = $('.lyrics-list');
 const endPoint = 'https://api.lyrics.ovh/suggest/';
 
 button.click(() => {
     $.ajax({
         url: `${endPoint}/${input.val()}`,
         success: resp => {
-            $.each(resp, (i, data) => {
-                console.log(data);
+            $.each(resp.data, (index, value) => {
+                lyricsList.append(`
+               <li class="list">
+                <span><strong>${value.artist.name}</strong> - ${value.title}</span>
+                <button class="btn" data-artist="${value.artist.name}" data-songtitle="${value.title}">Get Lyrics</button>
+              </li>
+                `);
             });
+            $('html, body').animate(
+                {
+                    scrollTop: $('#results').offset().top - 60,
+                },
+                1500
+            );
         },
     });
 });
